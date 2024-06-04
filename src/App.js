6b84058542ccc6361c54fad8e4904adb00e5d2fd
minji4252 <alswl7216@naver.com>
2024-06-04 14:11:37 +0900

    feat: calendar페이지 달력추가, 반응형 완성, 모달창 toggle기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import React from "react";
-import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
